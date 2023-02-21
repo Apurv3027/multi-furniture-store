e3c4925873833f04489d46feebac92014055ad3d
APURV PATEL <apurvp002@gmail.com>
2023-02-22 00:58:03 +0530

    Changes in File Favorite Product Model and Screen

---
+++ lib/models/favorite_product.dart
@@ -0,0 +1,12 @@
+class FavoriteProduct {
+  final String name;
+  final String price;
+  final String image;
+
+  FavoriteProduct({
+    required this.name,
+    required this.price,
+    required this.image,
+  });
+
+}
